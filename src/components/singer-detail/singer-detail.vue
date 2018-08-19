42684dc6670efaf9b4565038ea07530fbca1e171
smilecamo <smilecamo99@gmail.com>
2018-08-19 22:41:10 +0800

    songs

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,23 +1,10 @@
 <template>
   <transition name="slide">
-    <div class="song-detail">
-      <scroll class="song-scroll">
-          <ul>
-            <div class="song-img">
-              <img :src="song" alt="">
-            </div>
-            <li class="song-detail">
-              <div v-for="item of songs" :key='item.id'>
-                <img width="60" height="60" v-lazy="item.al.picUrl" alt="">
-                <span>{{item.name}}</span>
-              </div>
-            </li>
-          </ul>
-      </scroll>
-      <div class="loading-center" v-show='!songs.length'>
-        <loading></loading>
-      </div>
-    </div>
+  <music-list
+  :title='title'
+  :songs='songs'
+  :bg-image='bgImage'
+  ></music-list>
   </transition>
 </template>
 
@@ -25,11 +12,13 @@
 import { mapGetters } from 'vuex'
 import Loading from 'base/loading/loading'
 import Scroll from 'base/scroll/scroll'
+import MusicList from 'components/music-list/music-list'
 export default {
   data() {
     return {
-      song: '',
-      songs: []
+      bgImage: '',
+      songs: [],
+      title: ''
     }
   },
   computed: {
@@ -37,61 +26,33 @@ export default {
       'singer'
     ])
   },
-  created () {
+  mounted () {
     this.axios.get('/api/artists', {params: { id: this.singer.id }})
       .then((res) => {
-        this.song = res.data.artist.picUrl
+        this.bgImage = res.data.artist.picUrl
+        this.title = res.data.artist.name
         this.songs = res.data.hotSongs
-        console.log(this.song)
+        console.log(this.bgImage)
         console.log(this.songs)
+        console.log(this.title)
       })
     console.log(this.singer.id)
   },
   components: {
     Scroll,
-    Loading
+    Loading,
+    MusicList
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import "~common/stylus/variable"
-.song-detail
-  position fixed
-  z-index 100
-  top 0
-  left 0
-  right 0
-  bottom 0
-  height 100%
-  overflow hidden
-  background $color-background
-  .song-scroll
-    height 100%
-    .song
-      width 100%
-      height 100%
-      ul
-        height 100%
-        .song-img
-          width 100%
-          height 25%
-          img
-            width 100%
-            height 100%
-        .song-detail
-          margin-top 300px
-          height 100%
-          display flex
-          flex-direction: column
-          img
-          span
-            flex 1
-  .loading-center
-    position absolute
-    width 100%
-    top 50%
-    transform translateY(-50%)
+.loading-center
+  position absolute
+  width 100%
+  top 50%
+  transform translateY(-50%)
 .slide-enter-active,.slide-leave-active
   transition all 0.3s
 .slide-enter,.slide-leave-to
